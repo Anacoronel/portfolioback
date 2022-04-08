dea7d1a76e8a25cdbc84853d687f8e1d94464ee1
Ana Coronel <anacoronel@hotmail.com>
2022-04-08 13:12:34 -0300

    pom fixed

--- src/main/java/com/portfolio/portfolioback/security/dto/JwtDto.java
+++ src/main/java/com/portfolio/portfolioback/security/dto/JwtDto.java
@@ -1,16 +1,23 @@
 package com.portfolio.portfolioback.security.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+public class JwtDto {
+    private String token;
 
-
-
-    @Getter @Setter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public class JwtDto {
-        private String token;
+    public JwtDto() {
     }
+
+    public JwtDto(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+
+}
