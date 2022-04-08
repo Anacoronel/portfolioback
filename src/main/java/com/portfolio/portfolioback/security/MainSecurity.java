df77bcbb96e8f79cf0cd4844d7415dc46ead7eeb
Ana Coronel <anacoronel@hotmail.com>
2022-04-08 17:16:05 -0300

    pom fixed

--- src/main/java/com/portfolio/portfolioback/security/MainSecurity.java
+++ src/main/java/com/portfolio/portfolioback/security/MainSecurity.java
@@ -61,6 +61,8 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/auth/login").permitAll()
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
--- src/main/java/com/portfolio/portfolioback/security/jwt/JwtEntryPoint.java
+++ src/main/java/com/portfolio/portfolioback/security/jwt/JwtEntryPoint.java
@@ -1,15 +1,17 @@
 package com.portfolio.portfolioback.security.jwt;
 
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class JwtEntryPoint implements AuthenticationEntryPoint {
@@ -18,7 +20,13 @@ public class JwtEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException authException) throws IOException, ServletException {
-        logger.error("Fail in commence method");
+        logger.error("Fail in commence method", authException.getMessage()) ;
+        if(authException instanceof BadCredentialsException) {
+            res.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            Result invalidUserNameOrPassword = ControllerUtils.buildErrorResult(101, "Incorrect account or password");
+            res.getOutputStream().write(new ObjectMapper()
+                    .writeValueAsBytes(invalidUserNameOrPassword));
+        } else {
         res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "unauthorized");
     }
 }
