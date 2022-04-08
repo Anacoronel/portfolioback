4edca39e69946764dbdea4392a33c9482de4d947
Ana Coronel <anacoronel@hotmail.com>
2022-04-08 12:12:26 -0300

    pom fixed

--- src/main/java/com/portfolio/portfolioback/security/dto/LoginUser.java
+++ src/main/java/com/portfolio/portfolioback/security/dto/LoginUser.java
@@ -1,12 +1,16 @@
 package com.portfolio.portfolioback.security.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class LoginUser {
     @NotBlank
